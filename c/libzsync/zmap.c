59c5182792590d42535f9418eaf7799e89b14e17
Colin Phipps <cph@moria.org.uk>
2006-08-05 23:17:46 +0100

    Remove some unused vars, and use format string defines in zmap.c Improve commenting on zmap_to_compressed_ranges.

--- c/libzsync/zmap.c
+++ c/libzsync/zmap.c
@@ -25,6 +25,14 @@
 #endif
 
 #include "zmap.h"
+#include "format_string.h"
+
+/* This is a record of a checkpoint in a zlib stream - we have the bit position
+ * (yes, bit - zlib compresses input bytes down to a variable number of bits)
+ * and the corresponding output byte offset.
+ * blockcount is 0 if this bit position in the zlib stream is the start of a
+ *  zlib block, and is 1, 2, 3 etc for subsequent points that are in the same
+ *  zlib block. */
 
 struct zmapentry {
   long long inbits;
@@ -32,6 +40,7 @@ struct zmapentry {
   int blockcount;
 };
 
+/* Store all the zmapentry's as an array, and the # of entries */
 struct zmap {
   int n;
   struct zmapentry* e;
@@ -58,7 +67,7 @@ struct zmap* zmap_make(const struct gzblock* zb, int n)
       uint16_t ob = ntohs(zb[i].outbyteoffset);
 
       if (ob & GZB_NOTBLOCKSTART) { /* Not a zlib block start, just a mid-block mark */
-	ob &= ~GZB_NOTBLOCKSTART; bc++;
+        ob &= ~GZB_NOTBLOCKSTART; bc++;
       }
       else { bc = 0; }
 
@@ -86,70 +95,113 @@ off_t* zmap_to_compressed_ranges(const struct zmap* zm, off_t* byterange, int nr
 {
   int i,k;
   long long lastwroteblockstart_inbitoffset = 0;
-  int k_at_last_block = -1;
   off_t* zbyterange = malloc(2 * 2 * nrange * sizeof *byterange);
 
+  /* For each range of data that we want, work out a corresponding byte range
+   * in the compressed file that definitely includes the data in the target
+   * range.
+   * zbyterange[0,1] is the 1st byte range, zbyterange[2,3] the second, 
+   * ... and k is the number of ranges so far */
+
   for (i=0,k=0; i<nrange; i++) {
+    /* target range in the uncompressed data */
     long long start = byterange[2*i];
     long long end = byterange[2*i+1];
+    /* and the range in the compressed data which we are trying to calculate */
     long long zstart = -1;
     long long zend = -1;
+
     long long lastblockstart_inbitoffset = 0;
     int j;
     
+    /* Step through the blocks of compressed data;
+     * the zstart, zend vars are the main state for this loop:
+     *  zstart == -1: we are looking for the first compressed block containing data from our target range
+     *  zstart != -1, zend == -1: we found the starting block, now looking for the first compressed block that is outside the range that we need
+     *  zend == -1: we got the end block too, all done
+     *
+     * lastblockstart_inbitoffset is a record of the offset of the most recent
+     *  zlib block header - see comment in the loop to see what we need it for.
+     */
     for (j=0; j<zm->n && (zstart == -1 || zend == -1); j++) {
       register long long inbitoffset = zm->e[j].inbits;
       register long long outbyteoffset = zm->e[j].outbytes;
       
-      /* Is this the first block that comes after the start point - if so, the previous block header is the place to start (hence we don't update the lastblockstart_* values until after this) */
-      if (start < outbyteoffset && zstart == -1) {
-	if (j == 0) break;
-	zstart = zm->e[j-1].inbits;
-	// fprintf(stderr,"starting range at zlib block %d offset %lld.%lld\n",j-1,zstart/8,zstart%8);
-	if (lastwroteblockstart_inbitoffset != lastblockstart_inbitoffset) {
-	  /* The zbyteranges 0...k-1 have retrieved everything we need up to the start of the previous block - record this fact for the caller */
-	  k_at_last_block = k;
+      /* Is this the first block that comes after the start point - if so, the
+       * previous block is the place to start */
 
-	  zbyterange[2*k] = lastblockstart_inbitoffset/8;
-	  zbyterange[2*k+1] = (lastblockstart_inbitoffset/8) + 200;
-	  k++;
-	  lastwroteblockstart_inbitoffset = lastblockstart_inbitoffset;
-	}
+      if (start < outbyteoffset && zstart == -1) {
+        if (j == 0) break;
+        zstart = zm->e[j-1].inbits;
+
+        /* We need the zlib block header for range of compressed data
+         * - you can't decompress the data without knowing the huffman tree
+         * for this block of data.
+         * So, immediately add a range of at least
+         *  *** WARNING MAGIC NUMBER ***           200 bytes
+         * (which is a guess by me, I think the zlib header never exceeds that)
+         * covering the preceding zlib block header */
+
+        if (lastwroteblockstart_inbitoffset != lastblockstart_inbitoffset) {
+          zbyterange[2*k] = lastblockstart_inbitoffset/8;
+          zbyterange[2*k+1] = zbyterange[2*k] + 200;
+          k++;
+          lastwroteblockstart_inbitoffset = lastblockstart_inbitoffset;
+        }
       }
       
-      if (!(zm->e[j].blockcount)) { /* Block starts here */
-	lastblockstart_inbitoffset = inbitoffset;
+      /* We need to remember the most recent zlib block header, for the above.
+       * (Note this is after the section above, because the code above is
+       *  looking at the previous checkpoint, zm->e[j-1]. Only now do we worry
+       *  about data in zm->e[j] .)
+       * If blockcount == 0, this point in the compressed data is a block header */
+      if (zm->e[j].blockcount == 0) { /* Block starts here */
+        lastblockstart_inbitoffset = inbitoffset;
       }
 
       /* If we have passed the start, and we have now passed the end, then the end of this block is the end of the range to fetch. Special case end of stream, where the range libzsync knows about could extend beyond the range of the zlib stream. */
       if (start < outbyteoffset && (end <= outbyteoffset || j == zm->n - 1)) {
-	zend = inbitoffset;
-	// fprintf(stderr,"ending range at zlib block %d offset %lld.%lld\n",j,zend/8,zend%8);
+        zend = inbitoffset;
+        /* Leave adding zstart...zend to the ranges-to-fetch to outside the loop */
       }
     }
     if (zend == -1 || zstart == -1) {
-      fprintf(stderr,"Z-Map couldn't tell us how to find %lld-%lld\n",byterange[2*i],byterange[2*i+1]);
+      fprintf(stderr,"Z-Map couldn't tell us how to find " OFF_T_PF "-" OFF_T_PF "\n",byterange[2*i],byterange[2*i+1]);
       free(zbyterange);
       return NULL;
     }
+    
+    /* Finally, translate bits to bytes and store these in our list of ranges to get */
     zbyterange[2*k] = zstart/8;
-    /* Note +9 trailing bits, the offset is to the last huffman code inside the block, and each code is at most 9 bits */
     zbyterange[2*k+1] = (zend+7)/8;
     k++;
   }
 
+  /* The byte ranges in the compressed file can abut or overlap -
+   *  so we go through and consolidate any overlapping ranges. */
   for (i=0; i<k-1;) {
     if (zbyterange[2*i+1] >= zbyterange[2*(i+1)]) {
       // Ranges overlap, merge
-      // fprintf(stderr,"merging ranges %lld-%lld and %lld-%lld\n",zbyterange[2*i+0],zbyterange[2*i+1],zbyterange[2*i+2],zbyterange[2*i+3]);
-      /* Second range might be contained in the first */
+      /* The end of the first range need not be before the end of the
+       *  second, so this if () block is to set the end of the combined block
+       *  to the greater of the two.
+       * The start of the second block could be before the start of the first:
+       *  but this only occurs where the second range is a block header, and
+       *  the first range is some data that an earlier uncompressed range
+       *  needed out of the same block, in which case we are guaranteed that
+       *  the block header must have been requested earlier, and so the second
+       *  block here can be dropped anyway.
+       */
       if (zbyterange[2*i+1] < zbyterange[2*(i+1)+1])
-	zbyterange[2*i+1] = zbyterange[2*(i+1)+1];
-      /* Copy the rest of the array down */
+        zbyterange[2*i+1] = zbyterange[2*(i+1)+1];
+
+      /* Now eliminate zbyterange[2*(i+1) +0 and +1]; */
       memmove(&zbyterange[2*i+2],&zbyterange[2*i+4], 2*(k-2-i)*sizeof(zbyterange[0]));
       k--;
     } else i++;
   }
+
+  /* Return the # of ranges and the array of byte ranges we have built */
   *num = k;
   if (k > 0) {
     zbyterange = realloc(zbyterange, 2 * k * sizeof *zbyterange);
