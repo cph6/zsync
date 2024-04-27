58739260e09bc57f3a07d91f2da60a927f2c1153
Michał Drozd <michaldrozd@protonmail.ch>
2024-04-27 23:17:00 +0200

    Build zsync and zsyncmake using build.zig (#1)

---
+++ build.zig
@@ -0,0 +1,194 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const config = b.addConfigHeader(
+        .{
+            .style = .blank,
+            .include_path = "config.h",
+        },
+        .{
+            .HAVE_FSEEKO = 1,
+            .HAVE_GETADDRINFO = 1,
+            .HAVE_INTTYPES_H = 1,
+            .HAVE_LIBSOCKET = 1,
+            .HAVE_MEMCPY = 1,
+            .HAVE_MEMORY_H = 1,
+            .HAVE_MKSTEMP = 1,
+            .HAVE_PREAD = 1,
+            .HAVE_PWRITE = 1,
+            .HAVE_STDINT_H = 1,
+            .HAVE_STDLIB_H = 1,
+            .HAVE_STRINGS_H = 1,
+            .HAVE_STRING_H = 1,
+            .HAVE_SYS_STAT_H = 1,
+            .HAVE_SYS_TYPES_H = 1,
+            .HAVE_UNISTD_H = 1,
+            .H_ERRNO_DECLARED = 1,
+            .PACKAGE = "zsync",
+            .PACKAGE_BUGREPORT = "http://zsync.moria.org.uk/",
+            .PACKAGE_NAME = "zsync",
+            .PACKAGE_STRING = "zsync 0.6.2",
+            .PACKAGE_TARNAME = "zsync",
+            .PACKAGE_URL = "",
+            .PACKAGE_VERSION = "0.6.2",
+            .SIZEOF_OFF_T = 8,
+            .SIZEOF_SIZE_T = 8,
+            .STDC_HEADERS = 1,
+            .VERSION = "0.6.2",
+            ._XOPEN_SOURCE = 600,
+            ._BSD_SOURCE = 1,
+        },
+    );
+
+    const librcksum = b.addStaticLibrary(.{
+        .name = "librcksum",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    librcksum.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "librcksum/state.c",
+            "librcksum/hash.c",
+            "librcksum/range.c",
+            "librcksum/rsum.c",
+            "librcksum/md4.c",
+        },
+        .flags = &.{
+            "-std=c11",
+            "-DHAVE_CONFIG_H",
+        },
+    });
+
+    librcksum.addIncludePath(.{ .path = "./" });
+    librcksum.addConfigHeader(config);
+
+    const zlib = b.addStaticLibrary(.{
+        .name = "zlib",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    zlib.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "zlib/inflate.c",
+            "zlib/zutil.c",
+            "zlib/crc32.c",
+            "zlib/adler32.c",
+            "zlib/inftrees.c",
+            "zlib/deflate.c",
+            "zlib/trees.c",
+            "zlib/compress.c",
+        },
+        .flags = &.{
+            "-std=c11",
+        },
+    });
+
+    const libzsync = b.addStaticLibrary(.{
+        .name = "libzsync",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    libzsync.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "libzsync/zsync.c",
+            "libzsync/zmap.c",
+            "libzsync/sha1.c",
+        },
+        .flags = &.{
+            "-std=c11",
+            "-DHAVE_CONFIG_H",
+        },
+    });
+
+    libzsync.addIncludePath(.{ .path = "./" });
+    libzsync.addConfigHeader(config);
+
+    libzsync.linkLibrary(zlib);
+    libzsync.linkLibrary(librcksum);
+
+    const zzsync = b.addExecutable(.{
+        .name = "zsync",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    zzsync.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "client.c",
+            "progress.c",
+            "url.c",
+            "http.c",
+            "base64.c",
+        },
+        .flags = &.{
+            "-std=c11",
+            "-DHAVE_CONFIG_H",
+        },
+    });
+
+    zzsync.addConfigHeader(config);
+    zzsync.linkLibrary(libzsync);
+
+    const zsyncmake = b.addExecutable(.{
+        .name = "zsyncmake",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+
+    zsyncmake.addCSourceFiles(.{
+        .files = &[_][]const u8{
+            "make.c",
+            "makegz.c",
+            "progress.c",
+        },
+        .flags = &.{
+            "-std=c11",
+            "-DHAVE_CONFIG_H",
+        },
+    });
+    zsyncmake.addIncludePath(.{ .path = "./" });
+    zsyncmake.addConfigHeader(config);
+
+    zsyncmake.linkLibrary(zlib);
+    zsyncmake.linkLibrary(libzsync);
+
+    b.installArtifact(zzsync);
+    b.installArtifact(zsyncmake);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(zzsync);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
